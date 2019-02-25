2a3b52c23e56a635e4d4c8c38b8afa45a935ecad
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-02-25 17:13:14 -0500

    getting started 5.6

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   #get 'pages/hola'
 
-  #get 'welcome/index'
+  get 'welcome/index'
   #get 'hola/:id' => 'pages#hola'
-  resources :users
-  #resources :articles
-  #root 'welcome#index'
+  #resources :users
+  resources :articles
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
