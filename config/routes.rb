a966fd3c749807b15e2cb8e2cd08ffa10adc5498
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-02-26 16:36:48 -0500

    6.4

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   #get 'hola/:id' => 'pages#hola'
   #resources :users
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190225220457) do
+ActiveRecord::Schema.define(version: 20190226213435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20190225220457) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -73,4 +82,5 @@ ActiveRecord::Schema.define(version: 20190225220457) do
 
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "patients"
+  add_foreign_key "comments", "articles"
 end
